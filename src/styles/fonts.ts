1ee59b32115269176dfa47a5edfaaef4eac14339
Eduarda Terra <eduardapterra98@gmail.com>
2025-02-12 14:32:16 -0300

    fix: fonts import

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -9,37 +9,37 @@ export const fontPrimary = DM_Sans({
 export const fontSecondary = localFont({
   src: [
     {
-      path: "../styles/localFonts/CabinetGrotesk-Black.woff2",
+      path: "./localFonts/CabinetGrotesk-Black.woff2",
       weight: "900",
       style: "black",
     },
     {
-      path: "../styles/localFonts/CabinetGrotesk-Extrabold.woff2",
+      path: "./localFonts/CabinetGrotesk-Extrabold.woff2",
       weight: "800",
       style: "extrabold",
     },
     {
-      path: "../styles/localFonts/CabinetGrotesk-Bold.woff2",
+      path: "./localFonts/CabinetGrotesk-Bold.woff2",
       weight: "700",
       style: "bold",
     },
     {
-      path: "../styles/localFonts/CabinetGrotesk-Medium.woff2",
+      path: "./localFonts/CabinetGrotesk-Medium.woff2",
       weight: "500",
       style: "medium",
     },
     {
-      path: "../styles/localFonts/CabinetGrotesk-Regular.woff2",
+      path: "./localFonts/CabinetGrotesk-Regular.woff2",
       weight: "400",
       style: "regular",
     },
     {
-      path: "../styles/localFonts/CabinetGrotesk-Thin.woff2",
+      path: "./localFonts/CabinetGrotesk-Thin.woff2",
       weight: "300",
       style: "light",
     },
     {
-      path: "../styles/localFonts/CabinetGrotesk-ExtraLight.woff2",
+      path: "./localFonts/CabinetGrotesk-ExtraLight.woff2",
       weight: "200",
       style: "extralight",
     },
