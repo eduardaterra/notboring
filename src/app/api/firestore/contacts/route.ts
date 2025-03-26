8152e5dea0b54ef1fd4a104d135f36e212d9c81e
Eduarda Terra <eduardapterra98@gmail.com>
2025-03-26 12:33:22 -0300

    fix: remove serviceAccountPath from firestore initialization

--- src/app/api/firestore/contacts/route.ts
+++ src/app/api/firestore/contacts/route.ts
@@ -1,18 +1,10 @@
 // app/api/firestore/route.js
 import { Firestore } from "@google-cloud/firestore";
 import { NextRequest, NextResponse } from "next/server";
-import path from "path";
-
-// Caminho para o arquivo JSON da chave da conta de serviço
-const serviceAccountPath = path.join(
-  process.cwd(),
-  "./config/google-cloud.json"
-);
 
 // Inicialize o Firestore
 const firestore = new Firestore({
   projectId: process.env.NEXT_PUBLIC_FIRESTORE_PROJECT_ID,
-  keyFilename: serviceAccountPath,
 });
 
 export async function POST(request: NextRequest) {
