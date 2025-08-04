6f6ff923fd7b4be1a974659c081ba0cfc43ef4c5
Eduarda Terra <eduardapterra98@gmail.com>
2025-08-03 23:42:39 -0300

    feat: create dynamic site metadata, add favicon and opengraph image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,29 @@
+import { Metadata } from "next";
 import { SliceZone } from "@prismicio/react";
 import { createClient } from "@/prismicio";
 import { components } from "@/slices";
 import "./styles.scss";
+
+export async function generateMetadata(): Promise<Metadata> {
+  const prismic = createClient();
+  const document = await prismic.getByUID("landing_page", "first-lp");
+  return {
+    title:
+      document.data.meta_title ?? "NOTBORING - A Creative Company | London UK",
+    description:
+      document.data.meta_description ??
+      "Designers, strategists, and technologists building what moves people and business. We tune into culture and push it forward. No theatrics. Just work that works.",
+    openGraph: {
+      title:
+        document.data.meta_title ??
+        "NOTBORING - A Creative Company | London UK",
+      description:
+        document.data.meta_description ??
+        "Designers, strategists, and technologists building what moves people and business. We tune into culture and push it forward. No theatrics. Just work that works.",
+      images: [document.data.meta_image.url as string],
+    },
+  };
+}
 
 export default async function Home() {
   const prismic = createClient();
